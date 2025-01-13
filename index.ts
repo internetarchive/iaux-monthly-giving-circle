9ee6994931e999978501762b3d89137c08026590
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2025-01-13 12:43:54 -0800

    export receipts

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export { MonthlyGivingCircle } from './src/monthly-giving-circle';
 export type { anUpdate } from './src/monthly-giving-circle';
+export type { aReceipt } from './src/models/receipt';
+export { Receipt } from './src/models/receipt';
