40585a0e7f1dfe8346922d869eebf349cd3ab506
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2025-01-13 12:42:58 -0800

    receipt model

---
+++ src/models/receipt.ts
@@ -0,0 +1,26 @@
+export type aReceipt = {
+  id: string;
+  amount: number;
+  date: string;
+  isTest: boolean;
+  token: string;
+};
+export class Receipt {
+  id: string;
+
+  amount: number;
+
+  date: string;
+
+  isTest: boolean;
+
+  token: string;
+
+  constructor(receipt: aReceipt) {
+    this.id = receipt.id;
+    this.amount = receipt.amount;
+    this.date = receipt.date;
+    this.isTest = receipt.isTest;
+    this.token = receipt.token;
+  }
+}
