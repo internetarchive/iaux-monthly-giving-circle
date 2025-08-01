c4458fbbb18d9a4109d7009d1314931e1d723239
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2025-07-31 22:30:23 -0700

    open types for consumption WEBDEV-7576 (#30)

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 export { MonthlyGivingCircle } from './src/monthly-giving-circle';
 export type { APlanUpdate } from './src/monthly-giving-circle';
 export { Receipt } from './src/models/receipt';
+export type { AReceipt } from './src/models/receipt';
 export { MonthlyPlan } from './src/models/plan';
+export type { Plan, BtData } from './src/models/plan';
